cd9c8b7bf8f269adca7a883b661919c8e5ff9787
20221TIIMI0351 <evelynpo59@gmail.com>
2023-03-29 11:55:16 -0300

    Initial commit

---
+++ app/src/main/java/pereira/otavio/evelyn/lista/MainActivity.java
@@ -0,0 +1,14 @@
+package pereira.otavio.evelyn.lista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
