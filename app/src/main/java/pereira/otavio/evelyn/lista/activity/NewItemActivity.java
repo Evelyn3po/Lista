a2b2d0312f0f0ba067feb8280ab3c72895637a53
20221TIIMI0351 <evelynpo59@gmail.com>
2023-05-05 09:07:32 -0300

    Terminei a lista part 2, so falta acertar os erros da New Item Activity.

--- app/src/main/java/pereira/otavio/evelyn/lista/activity/NewItemActivity.java
+++ app/src/main/java/pereira/otavio/evelyn/lista/activity/NewItemActivity.java
@@ -1,8 +1,11 @@
 package pereira.otavio.evelyn.lista.activity;
 
+import static pereira.otavio.evelyn.lista.activity.MainActivity.NEW_ITEM_REQUEST;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -15,13 +18,19 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import pereira.otavio.evelyn.lista.R;
+import pereira.otavio.evelyn.lista.adapter.MyAdapter;
+import pereira.otavio.evelyn.lista.model.MainActivityViewModel;
+import pereira.otavio.evelyn.lista.model.MyItem;
 import pereira.otavio.evelyn.lista.model.NewItemActivityViewModel;
 
 public class NewItemActivity extends AppCompatActivity {
-
-    static int PHOTO_PICKER_REQUEST = 1;
-    Uri photoSelected = null;
+    MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,29 +39,40 @@ public class NewItemActivity extends AppCompatActivity {
 
         NewItemActivityViewModel vm = new ViewModelProvider(this).get(
                 NewItemActivityViewModel.class);
-
         Uri selectPhotoLocation = vm.getSelectPhotoLocation();
         if (selectPhotoLocation != null) {
             ImageView imvfotoPreview = findViewById(R.id.imvfotoPreview);
             imvfotoPreview.setImageURI(selectPhotoLocation);
+
         }
-        ImageButton imgCI = findViewById(R.id.imbCl);
+        ImageButton imgCL = findViewById(R.id.imbCl);
+
+        RecyclerView rvItens = findViewById(R.id.rvItens);
+        MainActivityViewModel vm = new ViewModelProvider(this).get(
+                MainActivityViewModel.class);
+        List<MyItem> items = vm.getItens();
+
+        myAdapter = new MyAdapter(this, itens);
+        rvItens.setAdapter(myAdapter);
 
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable
+    Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PHOTO_PICKER_REQUEST) {
             if (resultCode == Activity.RESULT_OK) {
-                photoSelected = data.getData();
-                ImageView imvfotoPreview =
-                        findViewById(R.id.imvfotoPreview);
+                Uri photoSelected = data.getData();
+                ImageView imvfotoPreview = findViewById(R.id.imvfotoPreview);
                 imvfotoPreview.setImageURI(photoSelected);
-                NewItemActivityViewModel vm = new ViewModelProvider( this).get( NewItemActivityViewModel.class );
+                NewItemActivityViewModel vm = new ViewModelProvider(this
+                ).get(NewItemActivityViewModel.class);
                 vm.setSelectPhotoLocation(selectedPhoto);
             }
         }
     }
 }
+
+
 
