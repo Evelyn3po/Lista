c06900d3dc3188f601d83142478621b23d47f791
20221TIIMI0351 <evelynpo59@gmail.com>
2023-05-10 09:10:02 -0300

    Terminei a lista part 1, so falta acertar o erro da NewItemactivity na classe ImgCl

--- app/src/main/java/pereira/otavio/evelyn/lista/activity/MainActivity.java
+++ app/src/main/java/pereira/otavio/evelyn/lista/activity/MainActivity.java
@@ -27,12 +27,29 @@ import pereira.otavio.evelyn.lista.util.Util;
 
 public class MainActivity extends AppCompatActivity {
     static int NEW_ITEM_REQUEST = 1;
+    List<MyItem> itens = new ArrayList<>();
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NEW_ITEM_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                MyItem myItem = new MyItem();
+                myItem.title = data.getStringExtra("title");
+                myItem.description =
+                        data.getStringExtra("description");
+                myItem.photo = data.getData();
+                itens.add(myItem);
+                itens.add(myItem);
+                myAdapter.notifyItemInserted(itens.size() - 1);
+            }
+        }
+    }
 
     MyAdapter myAdapter;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -44,41 +61,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(i, NEW_ITEM_REQUEST);
             }
         });
-        /*configurado na classe my Adapter*/
-        RecyclerView rvItens = findViewById(R.id.rvItens);
-        MainActivityViewModel vm = new ViewModelProvider(this).get(MainActivityViewModel.class);
-        List<MyItem> itens = vm.getItens();
 
+        RecyclerView rvItens = findViewById(R.id.rvItens);
         myAdapter = new MyAdapter(this, itens);
         rvItens.setAdapter(myAdapter);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == NEW_ITEM_REQUEST) {
-            if (resultCode == Activity.RESULT_OK) {
-
-                MyItem myItem = new MyItem();
-                myItem.title = data.getStringExtra("title");
-                myItem.description = data.getStringExtra("description");
-                Uri selectedPhotoURI = data.getData();
-                try {
-                    Bitmap photo = Util.getBitmap(MainActivity.this, selectedPhotoURI, 100, 100);
-                myItem.photo = photo; }
-
-                catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                MainActivityViewModel vm = new ViewModelProvider( this ).get(
-                        MainActivityViewModel.class );
-                List<MyItem> itens = vm.getItens();
-
-                itens.add(myItem);
-                myAdapter.notifyItemInserted(itens.size() - 1);
-            }
-        }
+        rvItens.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rvItens.setLayoutManager(layoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvItens.getContext(),
+                DividerItemDecoration.VERTICAL);
+        rvItens.addItemDecoration(dividerItemDecoration);
     }
 }
-
--- app/src/main/java/pereira/otavio/evelyn/lista/activity/NewItemActivity.java
+++ app/src/main/java/pereira/otavio/evelyn/lista/activity/NewItemActivity.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,51 +30,70 @@ import pereira.otavio.evelyn.lista.adapter.MyAdapter;
 import pereira.otavio.evelyn.lista.model.MainActivityViewModel;
 import pereira.otavio.evelyn.lista.model.MyItem;
 import pereira.otavio.evelyn.lista.model.NewItemActivityViewModel;
+import pereira.otavio.evelyn.lista.util.Util;
 
 public class NewItemActivity extends AppCompatActivity {
-    MyAdapter myAdapter;
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
 
-        NewItemActivityViewModel vm = new ViewModelProvider(this).get(
-                NewItemActivityViewModel.class);
-        Uri selectPhotoLocation = vm.getSelectPhotoLocation();
-        if (selectPhotoLocation != null) {
-            ImageView imvfotoPreview = findViewById(R.id.imvfotoPreview);
-            imvfotoPreview.setImageURI(selectPhotoLocation);
-
-        }
         ImageButton imgCL = findViewById(R.id.imbCl);
+        ImgCl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+            }
+        });
 
-        RecyclerView rvItens = findViewById(R.id.rvItens);
-        MainActivityViewModel vm = new ViewModelProvider(this).get(
-                MainActivityViewModel.class);
-        List<MyItem> items = vm.getItens();
-
-        myAdapter = new MyAdapter(this, itens);
-        rvItens.setAdapter(myAdapter);
-
+        Button btnAddItem = findViewById(R.id.btnAddItem);
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (photoSelected == null) {
+                    Toast.makeText(NewItemActivity.this, "É necessário selecionar uma imagem!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                EditText etTitle = findViewById(R.id.etTitle);
+                String title = etTitle.getText().toString();
+                if (title.isEmpty()) {
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir um título", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                EditText etDesc = findViewById(R.id.etDesc);
+                String description = etDesc.getText().toString();
+                if (description.isEmpty()) {
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir uma descrição", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Intent i = new Intent();
+                i.setData(photoSelected);
+                i.putExtra("title", title);
+                i.putExtra("description", description);
+                setResult(Activity.RESULT_OK, i);
+                finish();
+            }
+        });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable
-    Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PHOTO_PICKER_REQUEST) {
             if (resultCode == Activity.RESULT_OK) {
-                Uri photoSelected = data.getData();
+                photoSelected = data.getData();
                 ImageView imvfotoPreview = findViewById(R.id.imvfotoPreview);
                 imvfotoPreview.setImageURI(photoSelected);
-                NewItemActivityViewModel vm = new ViewModelProvider(this
-                ).get(NewItemActivityViewModel.class);
-                vm.setSelectPhotoLocation(selectedPhoto);
             }
         }
     }
 }
+
 
 
 
